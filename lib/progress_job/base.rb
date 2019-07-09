601276f4e312fa5426863f793e2e18af07c579fe
Dovi Winberger <dowi84@gmail.com>
2019-07-09 11:21:03 +0300

    move reset outside of before

--- lib/progress_job/base.rb
+++ lib/progress_job/base.rb
@@ -6,10 +6,15 @@ module ProgressJob
 
     def before(job)
       @job = job
+#       job.update_column(:progress_max, @progress_max)
+#       job.update_column(:progress_current, 0)
+    end
+  
+    def reset_rogress
       job.update_column(:progress_max, @progress_max)
       job.update_column(:progress_current, 0)
     end
-
+    
     def update_progress(step: 1)
       @job.update_column(:progress_current, @job.progress_current + step)
     end
